cb448ecc834835b90952584766aa81722eb52557
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-06 18:00:37 +0530

    Bubble.sort.java

---
+++ BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+    public static void bublesort(int nums[]) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int nums[]) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 5, 4, 1, 3, 2 };
+        bublesort(nums);
+        printArr(nums);
+    }
+}
